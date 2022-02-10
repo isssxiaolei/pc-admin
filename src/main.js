b4081537210434c28f1fb29c29c6b9a3f7d76770
xiaolei <18839268927@163.com>
2022-02-10 16:58:51 +0800

    配置element

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载element组件库
+import ElementUI from 'element-ui'
+// 加载element样式文件
+import 'element-ui/lib/theme-chalk/index.css'
 // 加载全局样式文件
 import './styles/index.less'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
